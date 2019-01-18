cb7fdf9f45b49b00a171fcfec607a98e46a871ab
Niels Poensgen <niels.poensgen@gmail.com>
2019-01-18 17:29:14 +0100

    tracking right user

--- gestures/gestureListener.js
+++ gestures/gestureListener.js
@@ -6,21 +6,23 @@ import { debugBreak, round } from './utils';
 const oldSpeed = 0;
 const userIsInformed = false;
 
-const whichBodyIsTracked = (bodyFrame) => {
+// returns the currently tracked body or false
+const trackedBody = (bodyFrame) => {
   let bodyTrackedIndex;
   bodyFrame.bodies.forEach((body) => {
     if (body.tracked) {
       bodyTrackedIndex = body.bodyIndex;
     }
   });
-  return bodyTrackedIndex || false;
+  if (bodyTrackedIndex) return bodyFrame.bodies[bodyTrackedIndex];
+  return false;
 };
 
 const gestureListener = (bodyFrame) => {
   if (DEBUG && debugBreak()) return;
+  const user = trackedBody(bodyFrame);
 
-  const user = bodyFrame.bodies[whichBodyIsTracked(bodyFrame.bodies)];
-  if (user.tracked) {
+  if (user) {
     console.log('user tracked');
     const rightHandX = user.joints[11].depthX;
     const leftHandX = user.joints[7].depthX;
